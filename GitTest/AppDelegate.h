0846ecaa7d2dc50acb5ba630f9930a24a8992cab
Peter Milway <peter@JoalahDesigns.com>
2011-12-25 15:08:32 -0500

    Initial Commit

---
+++ GitTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GitTest
+//
+//  Created by Peter Milway on 12/25/11.
+//  Copyright (c) 2011 Joalah Designs LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GitTest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  GitTest
+//
+//  Created by Peter Milway on 12/25/11.
+//  Copyright (c) 2011 Joalah Designs LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
